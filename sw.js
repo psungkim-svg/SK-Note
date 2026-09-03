146591062311461abec01967737172c0e9f2f316
psungkim-svg <psungkim@gmail.com>
2026-09-03 10:15:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* SK 노트 — Service Worker (오프라인 지원) */
-const CACHE = 'sknote-v27';
+const CACHE = 'sknote-v28';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'];
 
 self.addEventListener('install', e => {
